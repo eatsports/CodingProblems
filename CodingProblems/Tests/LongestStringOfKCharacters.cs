c1fbae25cae1e30e942e4dfe7d5b45b24dcb3d60
Kushat Sport <mousewell1@gmail.com>
2019-05-07 20:01:53 +0300

    Added Summary

--- CodingProblems/Tests/LongestStringOfKCharacters.cs
+++ CodingProblems/Tests/LongestStringOfKCharacters.cs
@@ -3,6 +3,10 @@ using NUnit.Framework;
 
 namespace CodingProblems.Tests
 {
+    /// <summary>
+    /// Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+    /// For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+    /// </summary>
     [TestFixture]
     public class LongestStringOfKCharacters
     {
